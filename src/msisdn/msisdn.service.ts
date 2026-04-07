55d850778b939aff81629e996e0347f0172a7b5f
tonyajicku <tony@citrakomunikasi.id>
2026-04-07 11:11:11 +0700

    update scrapping
    hanya yg blm habis yg di scrape

--- src/msisdn/msisdn.service.ts
+++ src/msisdn/msisdn.service.ts
@@ -289,6 +289,7 @@ export class MsisdnService {
 
     async getAllLinks() {
         return this.prisma.client.msisdn.findMany({
+            where: { isExhausted: false },
             select: { id: true, linkCekKuota: true, kuota: true, city: true },
         });
     }
